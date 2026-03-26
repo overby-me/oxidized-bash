15c0b69542a0cd7049aa9c00834de363c278e819
Niclas Overby <niclas@overby.me>
2026-03-26 18:03:35 +0100

    fix(rust/bash): Add compgen -A shopt support for listing all shopt option names
    Signed-off-by: Niclas Overby <niclas@overby.me>

--- src/builtins.rs
+++ src/builtins.rs
@@ -7268,6 +7268,75 @@ fn builtin_compgen(shell: &mut Shell, args: &[String]) -> i32 {
                 }
             }
         }
+        Some("shopt") => {
+            // List all shopt option names
+            let all_shopts = [
+                "array_expand_once",
+                "assoc_expand_once",
+                "autocd",
+                "bash_source_fullpath",
+                "cdable_vars",
+                "cdspell",
+                "checkhash",
+                "checkjobs",
+                "checkwinsize",
+                "cmdhist",
+                "compat31",
+                "compat32",
+                "compat40",
+                "compat41",
+                "compat42",
+                "compat43",
+                "compat44",
+                "complete_fullquote",
+                "direxpand",
+                "dirspell",
+                "dotglob",
+                "execfail",
+                "expand_aliases",
+                "extdebug",
+                "extglob",
+                "extquote",
+                "failglob",
+                "force_fignore",
+                "globasciiranges",
+                "globskipdots",
+                "globstar",
+                "gnu_errfmt",
+                "histappend",
+                "histreedit",
+                "histverify",
+                "hostcomplete",
+                "huponexit",
+                "inherit_errexit",
+                "interactive_comments",
+                "lastpipe",
+                "lithist",
+                "localvar_inherit",
+                "localvar_unset",
+                "login_shell",
+                "mailwarn",
+                "no_empty_cmd_completion",
+                "nocaseglob",
+                "nocasematch",
+                "noexpand_translation",
+                "nullglob",
+                "patsub_replacement",
+                "progcomp",
+                "progcomp_alias",
+                "promptvars",
+                "restricted_shell",
+                "shift_verbose",
+                "sourcepath",
+                "varredir_close",
+                "xpg_echo",
+            ];
+            for name in &all_shopts {
+                if prefix.is_empty() || name.starts_with(&prefix) {
+                    println!("{}", name);
+                }
+            }
+        }
         _ => {}
     }
     0
